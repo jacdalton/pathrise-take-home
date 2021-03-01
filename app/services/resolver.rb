533e30159e5cce6276bd2d47c154051173eb51ec
Jacque Dalton <jacque.m.dalton@gmail.com>
2021-02-28 16:28:15 -0800

    Implement resolve_company_website and resolve_one methods

--- app/services/resolver.rb
+++ app/services/resolver.rb
@@ -10,8 +10,23 @@ class Resolver
 
   def resolve_one(job)
     # job param should be instance of JobOpportunity & have company_name and job_url attributes
+    # Initialize job_source to unknown (should return this if other checks fail)
+    job_source = "Unknown"
     # resolve_jb will be either job board name or nil
     resolve_jb = resolve_job_board(job.job_url)
+    if resolve_jb.nil?
+      # If resolve_jb is nil, no job board was found so we check for presence of company name in URL
+      resolve_company = resolve_company_website(job)
+      if resolve_company
+        # If company name is found in URL, set job source to Company Website
+        job_source = "Company Website"
+      end
+    else
+      # if resolve_jb is not nil, set job_source to the job board name
+      job_source = resolve_jb
+    end
+
+    return job_source
   end
 
   private
@@ -27,5 +42,10 @@ class Resolver
     end
     # job_board should either be a job board's name from the JSON file or nil if the root_domain was not found
     return job_board
+  end
+
+  def resolve_company_website(job)
+    # [[:space:]] will account for whitespaces as well as &nbsp;
+    return job.job_url.downcase.include? job.company_name.downcase.gsub(/[[:space:]]+/, "")
   end
 end
