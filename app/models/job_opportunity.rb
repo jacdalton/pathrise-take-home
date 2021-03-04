01d90d5b6ef1cfb4d5bf1ec437f23583fd8017b2
Jacque Dalton <jacque.m.dalton@gmail.com>
2021-03-03 18:57:37 -0800

    Comment out ignored columns in model to run seed in Heroku

--- app/models/job_opportunity.rb
+++ app/models/job_opportunity.rb
@@ -1,4 +1,4 @@
 class JobOpportunity < ApplicationRecord
   # Don't have a need to include the timestamp columns & they clutter up display so exclude them for now
-  self.ignored_columns = %w[created_at updated_at]
+  # self.ignored_columns = %w[created_at updated_at]
 end
